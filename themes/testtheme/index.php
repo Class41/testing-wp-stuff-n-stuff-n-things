582f4c6390e99195ef5f46eee56a9cecc472f8ff
class41 <projectclass41@gmail.com>
2019-06-07 08:07:14 -0500

    initial stuff

--- themes/testtheme/index.php
+++ themes/testtheme/index.php
@@ -6,17 +6,17 @@ get_header();
 
 <div id="body-container">
 
-    <form method="post" action="<?php echo esc_url( admin_url('admin-post.php') );?>">
+<form method="post" action="<?php echo esc_url( admin_url('admin-post.php') );?>">
         <label>Email</label>
-        <input type="text" name="email"/> <br/>
+        <input type="email" name="email"/> <br/>
         <label>Username</label>
         <input type="text" name="username"/><br/>
         <label>Password</label>
-        <input type="text" name="password"/><br/>
+        <input type="password" name="password"/><br/>
         <input type="hidden" value="submit" action="register"/>
         <input type="hidden" name="_nonce" value="<?php echo wp_create_nonce( '_nonce' ); ?>"/>
         <input type="submit"/>
-    </form>
+</form>
 
 </div>
 
